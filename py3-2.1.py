00224072045be9600c045170f25776a6a4ee5c9b
Vsevolod <vs.kamyshev@gmail.com>
2017-03-04 13:22:34 +0300

    Добавлено чтение и загрузка данных из файла

--- py3-2.1.py
+++ py3-2.1.py
@@ -1,20 +1,42 @@
-cook_book = {
-  'яйчница': [
-    {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-    ],
-  'стейк': [
-    {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-    {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},    
-    {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
-    ],
-  'салат': [
-    {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-    {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},    
-    {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-    {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-    ]
-  }
+# cook_book = {
+#   'яйчница': [
+#     {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
+#     {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
+#     ],
+#   'стейк': [
+#     {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
+#     {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
+#     {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
+#     ],
+#   'салат': [
+#     {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
+#     {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
+#     {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
+#     {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
+#     ]
+#   }
+
+#Делаем загрузку таблицы из файла
+cook_book ={}
+with open('recipes.txt', 'r') as f:
+    file_end = False
+    while file_end == False:
+        recipe = f.readline().strip()
+        print(recipe)
+        num = int(f.readline().strip())
+        ingridient_list = []
+        while num > 0:
+            ingridients = f.readline().strip()
+            ingridients = ingridients.split(' | ')
+            ingridient_list.append({'ingridient_name': ingridients[0], 'quantity': int(ingridients[1]), 'measure': ingridients[2]})
+            num -= 1
+        cook_book[recipe] = ingridient_list
+        print(cook_book)
+
+        if f.readline():
+            file_end = False
+        else:
+            file_end = True
 
 def get_shop_list_by_dishes(dishes, person_count):
   shop_list = {}
@@ -28,17 +50,16 @@ def get_shop_list_by_dishes(dishes, person_count):
         shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
   return shop_list
 
-def print_shop_list(shop_list):  
+def print_shop_list(shop_list):
   # for shop_list_item in shop_list.values():
   #   print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
   for shop_list_item in shop_list.values():
     print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
-    
+
 def create_shop_list():
   person_count = int(input('Введите количество человек: '))
-  dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
+  dishes = input('Введите блюда в расчете на одного человека (через запятую): ').split(', ')
   shop_list = get_shop_list_by_dishes(dishes, person_count)
   print_shop_list(shop_list)
-  
+
 create_shop_list()
-    
