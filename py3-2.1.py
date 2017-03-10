feb5a2d469e379734c56f629694131e8a5be6276
Vsevolod <vs.kamyshev@gmail.com>
2017-03-10 23:47:32 +0300

    Внесены правки. Доделана декомпозиция. Убран отладочный вывод.

--- py3-2.1.py
+++ py3-2.1.py
@@ -1,47 +1,31 @@
-# cook_book = {
-#   'яйчница': [
-#     {'ingridient_name': 'яйца', 'quantity': 2, 'measure': 'шт.'},
-#     {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'}
-#     ],
-#   'стейк': [
-#     {'ingridient_name': 'говядина', 'quantity': 300, 'measure': 'гр.'},
-#     {'ingridient_name': 'специи', 'quantity': 5, 'measure': 'гр.'},
-#     {'ingridient_name': 'масло', 'quantity': 10, 'measure': 'мл.'}
-#     ],
-#   'салат': [
-#     {'ingridient_name': 'помидоры', 'quantity': 100, 'measure': 'гр.'},
-#     {'ingridient_name': 'огурцы', 'quantity': 100, 'measure': 'гр.'},
-#     {'ingridient_name': 'масло', 'quantity': 100, 'measure': 'мл.'},
-#     {'ingridient_name': 'лук', 'quantity': 1, 'measure': 'шт.'}
-#     ]
-#   }
-
 #Делаем загрузку таблицы из файла
-cook_book ={}
-with open('recipes.txt', 'r') as f:
-    file_end = False
-    while file_end == False:
-        recipe = f.readline().strip()
-        print(recipe)
-        num = int(f.readline().strip())
-        ingridient_list = []
-        while num > 0:
-            ingridients = f.readline().strip()
-            ingridients = ingridients.split(' | ')
-            ingridient_list.append({'ingridient_name': ingridients[0], 'quantity': int(ingridients[1]), 'measure': ingridients[2]})
-            num -= 1
-        cook_book[recipe] = ingridient_list
-        print(cook_book)
+def cook_book():
+    cook_book = {}
+    with open('recipes.txt', 'r') as f:
+        file_end = False
+        while file_end == False:
+            recipe = f.readline().strip()
+            num = int(f.readline().strip())
+            ingridient_list = []
 
-        if f.readline():
-            file_end = False
-        else:
-            file_end = True
+            for _ in range(num):
+                ingridients = f.readline().strip()
+                ingridients = ingridients.split(' | ')
+                ingridient_list.append({'ingridient_name': ingridients[0], 'quantity': int(ingridients[1]), 'measure': ingridients[2]})
+                num -= 1
 
-def get_shop_list_by_dishes(dishes, person_count):
+            cook_book[recipe] = ingridient_list
+            if f.readline():
+                file_end = False
+            else:
+                file_end = True
+    return (cook_book)
+
+#Формирование списка покупок
+def get_shop_list_by_dishes(dishes, recipes_book, person_count):
   shop_list = {}
   for dish in dishes:
-    for ingridient in cook_book[dish]:
+    for ingridient in recipes_book[dish]:
       new_shop_list_item = dict(ingridient)
       new_shop_list_item['quantity'] *= person_count
       if new_shop_list_item['ingridient_name'] not in shop_list:
@@ -50,16 +34,13 @@ def get_shop_list_by_dishes(dishes, person_count):
         shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
   return shop_list
 
+#Вывод списка покупок
 def print_shop_list(shop_list):
-  # for shop_list_item in shop_list.values():
-  #   print('{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
-  for shop_list_item in shop_list.values():
-    print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
+    for shop_list_item in shop_list.values():
+        print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
 
-def create_shop_list():
-  person_count = int(input('Введите количество человек: '))
-  dishes = input('Введите блюда в расчете на одного человека (через запятую): ').split(', ')
-  shop_list = get_shop_list_by_dishes(dishes, person_count)
-  print_shop_list(shop_list)
 
-create_shop_list()
+person_count = int(input('Введите количество человек: '))
+dishes = input('Введите блюда в расчете на одного человека (через запятую): ').split(', ')
+shop_list = get_shop_list_by_dishes(dishes, cook_book(), person_count)
+print_shop_list(shop_list)
